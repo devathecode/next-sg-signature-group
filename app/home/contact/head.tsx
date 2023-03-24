6db0060c1966f827c435f912ff874a853fd6e79a
Devanshu <devanshuv1501@gmail.com>
2023-03-25 00:45:43 +0530

    Added temporrary footer section

---
+++ app/home/contact/head.tsx
@@ -0,0 +1,11 @@
+
+const Head = () => {
+    return (
+        <>
+            <title>Contact Us | SG Signature Group</title>
+            <meta name="description" content="Let's talk about everything"/>
+        </>
+    );
+};
+
+export default Head;
--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -3,6 +3,7 @@
 import React, {useState} from 'react';
 import Image from "next/image";
 import Header from "@/app/core/components/Header/Header";
+import Footer from "@/app/core/components/Footer/Footer";
 
 export default function HomeLayout({
                                        children,
@@ -16,6 +17,7 @@ export default function HomeLayout({
             <div className="mt-12 md:mt-16">
                 {children}
             </div>
+            <Footer/>
         </div>
     )
 }
