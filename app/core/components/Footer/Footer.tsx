7a0a82b7ea020dac8a3e9341f0247118f7e91937
Devanshu <devanshuv1501@gmail.com>
2023-03-25 13:27:35 +0530

    Updated footer

--- app/core/components/Footer/Footer.tsx
+++ app/core/components/Footer/Footer.tsx
@@ -1,151 +1,40 @@
 import React from 'react';
+import Image from "next/image";
 
 const Footer = () => {
     return (
-        <footer className="footer-1 bg-gray-100 py-8 sm:py-12">
-            <div className="container mx-auto px-4">
-                <div className="sm:flex sm:flex-wrap sm:-mx-4 md:py-4">
-                    <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6">
-                        <h5 className="text-xl font-bold mb-6">Features</h5>
-                        <ul className="list-none footer-links">
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Cool
-                                    stuff</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Random
-                                    feature</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Team
-                                    feature</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Stuff
-                                    for developers</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Another
-                                    one</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Last
-                                    time</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 sm:mt-0">
-                        <h5 className="text-xl font-bold mb-6">Resources</h5>
-                        <ul className="list-none footer-links">
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Resource</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Resource
-                                    name</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Another
-                                    resource</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Final
-                                    resource</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
-                        <h5 className="text-xl font-bold mb-6">About</h5>
-                        <ul className="list-none footer-links">
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Team</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Locations</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Privacy</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Terms</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6 mt-8 md:mt-0">
-                        <h5 className="text-xl font-bold mb-6">Help</h5>
-                        <ul className="list-none footer-links">
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Support</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Help
-                                    Center</a>
-                            </li>
-                            <li className="mb-2">
-                                <a href="#"
-                                   className="border-b border-solid border-transparent hover:border-purple-800 hover:text-purple-800">Contact
-                                    Us</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="px-4 mt-4 sm:w-1/3 xl:w-1/6 sm:mx-auto xl:mt-0 xl:ml-auto">
-                        <h5 className="text-xl font-bold mb-6 sm:text-center xl:text-left">Stay connected</h5>
-                        <div className="flex sm:justify-center xl:justify-start">
-                            <a href=""
-                               className="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 text-gray-600 hover:text-white hover:bg-blue-600 hover:border-blue-600">
-                                <i className="fab fa-facebook"></i>
-                            </a>
-                            <a href=""
-                               className="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-blue-400 hover:border-blue-400">
-                                <i className="fab fa-twitter"></i>
-                            </a>
-                            <a href=""
-                               className="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-red-600 hover:border-red-600">
-                                <i className="fab fa-google-plus-g"></i>
-                            </a>
+        <div className="bg-gray-100 py-10">
+            <div className="grid grid-cols-12 max-w-6xl mx-auto">
+                <div className="col-span-6">
+                    <div className="mb-4 font-mono flex flex-row items-center">
+                        <Image width="200" height="80" src="/images/sg-logo2.png" className="w-10 md:w-12 h-auto" alt=""/>
+                        <div className="flex flex-col">
+                                <span className="text-lg sm:text-xl md:text-2xl font-semibold tracking-tighter text-black">
+                                    Signature
+                                </span>
+                            <span className="italic text-xs text-black -mt-1 tracking-widest">Group</span>
                         </div>
                     </div>
-                </div>
 
-                <div className="sm:flex sm:flex-wrap sm:-mx-4 mt-6 pt-6 sm:mt-12 sm:pt-12 border-t">
-                    <div className="sm:w-full px-4 md:w-1/6">
-                        <strong>FWR</strong>
+                    <div className="flex text-gray-500 uppercase text-xs">
+                        Address : Buddh Vihar Part - A 35/2-B Taramandal, Siddharth Enclave
+                        Infront of Post Office, Gorakhpur-273017
                     </div>
-                    <div className="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
-                        <h6 className="font-bold mb-2">Address</h6>
-                        <address className="not-italic mb-4 text-sm">
-                            123 6th St.<br/>
-                            Melbourne, FL 32904
-                        </address>
-                    </div>
-                    <div className="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
-                        <h6 className="font-bold mb-2">Free Resources</h6>
-                        <p className="mb-4 text-sm">Use our HTML blocks for <strong>FREE</strong>.<br/>
-                            <em>All are MIT License</em></p>
-                    </div>
-                    <div className="px-4 md:w-1/4 md:ml-auto mt-6 sm:mt-4 md:mt-0">
-                        <button className="px-4 py-2 bg-yellow-600 hover:bg-yellow-700 rounded text-white">Get Started
-                        </button>
-                    </div>
+                </div>
+                <div className="col-span-6 place-self-end">
+                    <h3 className="font-medium text-lg text-yellow-600 mb-4">Subscribe to our Newsletter</h3>
+                    <form className="mt-4">
+                        <input
+                            className="border rounded w-full px-4 py-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type="email" placeholder="username@email.com"/>
+                    </form>
+                </div>
+                <div className="col-span-12 border-t border-gray-300 mt-10 text-gray-500">
+                    <p className="mt-6 text-center">© 2023 SG Signature Group. All rights reserved.
+                    </p>
                 </div>
             </div>
-        </footer>
+        </div>
     );
 };
 
---
+++ app/home/properties/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            properties
+        </div>
+    );
+};
+
+export default Page;
